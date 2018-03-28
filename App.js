2e070cf92df27bc97ce5aa6b49ba1ed11a381c3e
seokrae <kslbsh@gmail.com>
2018-03-28 14:39:35 +0900

    #6 Styling the To Do Component

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { StyleSheet, 
+        Text, 
+        View, 
+        StatusBar, 
+        TextInput, 
+        Dimensions, 
+        Platform,
+        ScrollView
+      } from 'react-native';
+import ToDo from './ToDo';
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
   render() {
-    return (
-      <View style={styles.container}>
+    const { newToDo } = this.state;
+    return <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>SR To Do</Text>
-        <View style={styles.card} >
-          <TextInput style={styles.input} placeholder={"New To Do"} />
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"} value={newToDo} onChangeText={this._controllNewToDo} placeholderTextColor={"#999"} returnKeyType={"done"} autoCorrect={false} />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo />
+          </ScrollView>
         </View>
-      </View>
-    );
+      </View>;
   }
+  _controllNewToDo = text => {
+    this.setState ({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -38,7 +57,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 10,
     ...Platform.select({
       ios: {
-        shadowColor: 'rgb(50,50,50)',
+        shadowColor: "rgb(50,50,50)",
         shadowOpacity: 0.5,
         shadowRadius: 5,
         shadowOffset: {
@@ -50,5 +69,14 @@ const styles = StyleSheet.create({
         elevation: 3
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25,
+  },
+  toDos: {
+    alignItems: "center"
   }
 });
